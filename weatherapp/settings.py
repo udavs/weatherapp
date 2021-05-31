18a2c507670c743d26fdfb8e7f399d95b499e3ca
Darya <udavdash@gmail.com>
2021-05-31 18:46:35 +0200

    Set SECRET_KEY envvar

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e+b5bkiu2+x4t@agf71@d9oo00*b4+sx++q&#t_ay%2$cm18y%"
+SECRET_KEY = os.getenv("SECRET_KEY", "secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
